30b69e326b2e1ece84e8a893b9678e050111a3ab
Brad Ruud <bradruud@gmail.com>
2020-05-03 23:12:35 +0000

    in progress

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,15 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find(params[:id]) 
   end
 
+  def orders_page
+    line_items = @order.line_items
+    @products = Product.joins(:line_items)
+    puts @product
+  end
+  
   def create
     charge = perform_stripe_charge
     order  = create_order(charge)
